c2398d8d674b5c6010b6a27bbdec86baa7ae75a0
Kamran Wali <kamran369mars@gmail.com>
2023-05-31 18:26:22 +0100

    Fixed manager helper null ref bug

--- CodeOptPro/Assets/KamranWali/CodeOptPro/Scripts/Managers/MonoAdvManager_Call.cs
+++ CodeOptPro/Assets/KamranWali/CodeOptPro/Scripts/Managers/MonoAdvManager_Call.cs
@@ -14,14 +14,19 @@ namespace KamranWali.CodeOptPro.Managers
 
         private void Awake()
         {
-            for (_counter = 0; _counter < _managers.Length; _counter++) _managers_PreAwakeAdv_Setup[_counter].PreAwakeAdv(); // Calling pre awake adv setup to allow global variable setup
+            for (_counter = 0; _counter < _managers_PreAwakeAdv_Setup.Count; _counter++) _managers_PreAwakeAdv_Setup[_counter].PreAwakeAdv(); // Calling pre awake adv setup to allow global variable setup
             for (_counter = 0; _counter < _managers.Length; _counter++) _managers[_counter].AwakeAdv(); // Calling all manager awake
         }
         private void Start() { for (_counter = 0; _counter < _managers.Length; _counter++) _managers[_counter].StartAdv(); } // Calling all manager start
 
         #region Editor Script
         public void AddObject(MonoAdvManager obj) => _managers_PreAwakeAdv_Setup.Add(obj);
-        public void ResetData() => _managers_PreAwakeAdv_Setup = new List<MonoAdvManager>();
+        
+        public void ResetData()
+        {
+            _managers_PreAwakeAdv_Setup = new List<MonoAdvManager>();
+            for (_counter = 0; _counter < _managers.Length; _counter++) _managers[_counter].SetManager(null); // Flushing out any null references
+        }
         #endregion
     }
 }
